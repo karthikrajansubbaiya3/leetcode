926baede8c3cc730e83738351bf9424b1196309c
karthikrajansubbaiya3 <107788148+karthikrajansubbaiya3@users.noreply.github.com>
2025-10-11 21:16:45 +0530

    day4
    dsa

--- leetdata.py
+++ leetdata.py
@@ -13,4 +13,27 @@ def sorted_array(array):
     return res
 
 
-print(sorted_array([-4,1,3,5]))
+#print(sorted_array([-4,1,3,5]))
+# time complexity of this is o(n)
+# because brute force we are traversing array by in timeline integer when go left value is increasing when we go right value is increasing
+# so by this square sorted array we can use 0 as right element n-1 as left element it is only for ascending order 
+def sorted_squaredarray(array):
+   n=len(array)
+   i,j=0,len(array)-1
+   res=[0]*n
+   for k in reversed(range(n)):
+      sq_i=array[i]**2
+      sq_j=array[j]**2
+      if(sq_i>sq_j):
+         res[k]=sq_i
+         i+=1
+      else:
+         res[k]=sq_j
+         j-=1
+   return res
+  
+
+      
+
+
+print(sorted_squaredarray([-5,-4,-2, 1, 9,12]))
